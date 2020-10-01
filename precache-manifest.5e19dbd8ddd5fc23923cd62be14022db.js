fccfd3d47dd126b9e01a17da40e4587dcc42b06c
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2020-10-01 12:50:36 -0400

    Updates

---
+++ precache-manifest.5e19dbd8ddd5fc23923cd62be14022db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16751449c1604080f5c412144e38368e",
+    "url": "/bystranowska.world/index.html"
+  },
+  {
+    "revision": "2b7da5e5174ab22d6dfb",
+    "url": "/bystranowska.world/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "1da69d766a630181d11e",
+    "url": "/bystranowska.world/static/js/2.2fcec213.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/bystranowska.world/static/js/2.2fcec213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7da5e5174ab22d6dfb",
+    "url": "/bystranowska.world/static/js/main.61b2537a.chunk.js"
+  },
+  {
+    "revision": "db2b317397c620c43628",
+    "url": "/bystranowska.world/static/js/runtime-main.104564fb.js"
+  }
+]);
